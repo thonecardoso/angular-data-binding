a6c3a91113346c02995c3591caa2a8fb2cece5ad
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-05-08 09:06:58 -0300

    Two way data-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DataBindingComponent} from './data-binding/data-binding.component';
+import {FormsModule} from '@angular/forms';
+import {MeuFormModule} from './meu-form/meu-form.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MeuFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -14,6 +14,7 @@ export class DataBindingComponent implements OnInit {
   isMouseOver = false;
 
 
+
   constructor() {
   }
 
