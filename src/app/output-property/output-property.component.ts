0b7e1bdde98aa561d80c7a383981b1e1ebe6b454
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-05-14 08:56:18 -0300

    ElementRef

--- src/app/output-property/output-property.component.ts
+++ src/app/output-property/output-property.component.ts
@@ -1,30 +1,35 @@
-import {Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component, Input, OnInit, EventEmitter, Output, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'contador',
   templateUrl: './output-property.component.html',
   styleUrls: ['./output-property.component.scss']
 })
-export class OutputPropertyComponent implements OnInit {
+export class OutputPropertyComponent implements AfterViewInit {
 
   @Input() valor = 0;
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput!: ElementRef;
+
 
   incrementa(): void {
-    this.valor++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   decrementa(): void {
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   constructor() {
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    console.log(this.campoValorInput);
   }
+
+
 
 }
